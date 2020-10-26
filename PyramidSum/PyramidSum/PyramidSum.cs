c72b2d1283890a3145a23073bf0ff18e91a6d9ef
shmirgelis <73270957+shmirgelis@users.noreply.github.com>
2020-10-26 07:58:22 +0200

    Add files via upload

---
+++ PyramidSum/PyramidSum/PyramidSum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PyramidSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
